bfe352d3d6db399e0bdd1ecdf6bdee272f7317d9
Exadra37 <exadra37@gmail.com>
2019-05-02 20:55:53 +0100

    Implementing Agents > Test setup with exunit callbacks: https://elixir-lang.org/getting-started/mix-otp/agent.html#test-setup-with-exunit-callbacks
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -1,12 +1,16 @@
 defmodule KV.BucketTest do
     use ExUnit.Case, async: true
 
-    test "stores values by key" do
+    setup do
         {:ok, bucket} = KV.Bucket.start_link([])
+        %{bucket: bucket}
+    end
+
+    test "stores values by key", %{bucket: bucket} do
+        # `bucket` is now the bucket from the setup block
         assert KV.Bucket.get(bucket, "milk") == nil
 
         KV.Bucket.put(bucket, "milk", 3)
         assert KV.Bucket.get(bucket, "milk") == 3
     end
-
 end
