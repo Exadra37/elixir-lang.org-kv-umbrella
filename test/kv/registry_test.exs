32572090d38965724a67c830654c2d75e38ba393
Exadra37 <exadra37@gmail.com>
2019-05-03 20:59:41 +0100

    Implememting our First GenServer > testing a GenServer: https://elixir-lang.org/getting-started/mix-otp/genserver.html#testing-a-genserver
    Signed-off-by: Exadra37 <exadra37@gmail.com>

---
+++ test/kv/registry_test.exs
@@ -0,0 +1,21 @@
+defmodule KV.RegistryTest do
+   use ExUnit.Case, async: true
+
+    setup do
+        # using start_supervised!/1 to start the process guarantees that ExUnit
+        # shuts down the KV.Registry process before it starts a new one for the
+        # next test, thus not leaking state between tests.
+        registry = start_supervised!(KV.Registry)
+        %{registry: registry}
+    end
+
+    test "spawns buckets", %{registry: registry} do
+        assert KV.Registry.lookup(registry, "shopping") == :error
+
+        KV.Registry.create(registry, "shopping")
+        assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+        KV.Bucket.put(bucket, "milk", 1)
+        assert KV.Bucket.get(bucket, "milk") == 1
+    end
+end
