05655e22a58a630eb1dea5d6393e34fe27ebf22f
Exadra37 <exadra37@gmail.com>
2019-05-04 10:04:18 +0100

    Implementing Supervisor and Application > Naming processes: https://elixir-lang.org/getting-started/mix-otp/supervisor-and-application.html#naming-processes
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -6,10 +6,12 @@ defmodule KV.Supervisor do
     end
 
     def init(:ok) do
-        children = {
-            KV.Registry
-        }
+        children = [
+            {KV.Registry, name: KV.Registry}
+        ]
 
+        # the strategy `:one_for_one` means that only the child that stops or
+        # crashes is restarted.
         Supervisor.init(children, strategy: :one_for_one)
     end
 end
