fe084a81d29139b09f0e7627879cb29ce6bf5c43
Exadra37 <exadra37@gmail.com>
2019-05-04 16:09:32 +0100

    Implementing Dynamic Supervisor > Supervision trees: https://elixir-lang.org/getting-started/mix-otp/dynamic-supervisor.html#supervision-trees
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -6,13 +6,20 @@ defmodule KV.Supervisor do
     end
 
     def init(:ok) do
+
+        # The children order is important.
+        # We want the Bucket to start before the Registry, because the Registry
+        # may try to call the Bucket.
         children = [
-            {KV.Registry, name: KV.Registry},
+
+            # The strategy `:one_for_one` means that only the child that stops
+            # or crashes is restarted.
             {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
+            {KV.Registry, name: KV.Registry},
         ]
 
-        # the strategy `:one_for_one` means that only the child that stops or
-        # crashes is restarted.
-        Supervisor.init(children, strategy: :one_for_one)
+        # The strategy `one_for_all` means that if one of the children dies, all
+        # children must be restarted.
+        Supervisor.init(children, strategy: :one_for_all)
     end
 end
