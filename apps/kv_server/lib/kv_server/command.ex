4d054d23493769adc13d8f375042f0fd46abf35c
Exadra37 <exadra37@gmail.com>
2019-05-09 22:29:09 +0100

    Implementing Doctests, Patterns and With > Running commands: https://elixir-lang.org/getting-started/mix-otp/docs-tests-and-with.html#running-commands
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -38,7 +38,44 @@ defmodule KVServer.Command do
     end
   end
 
-  def run(command) do
+  @doc """
+  Runs the given command.
+  """
+  # Functions without a body serve only for documentation purpose... to tell us
+  # what are the arguments in a scenario were we have multiple pattern matching
+  # on  same function.
+  def run(command)
+
+  def run({:create, bucket}) do
+    KV.Registry.create(KV.Registry, bucket)
     {:ok, "OK\r\n"}
+  end
+
+  def run({:get, bucket, key}) do
+    lookup(bucket, fn pid ->
+      value = KV.Bucket.get(pid, key)
+      {:ok, "#{value}\r\nOK\r\n"}
+    end)
+  end
+
+  def run({:put, bucket, key, value}) do
+    lookup(bucket, fn pid ->
+      KV.Bucket.put(pid, key, value)
+      {:ok, "OK\r\n"}
+    end)
+  end
+
+  def run({:delete, bucket, key}) do
+    lookup(bucket, fn pid ->
+      KV.Bucket.delete(pid, key)
+      {:ok, "OK\r\n"}
+    end)
+  end
+
+  defp lookup(bucket, callback) do
+    case KV.Registry.lookup(KV.Registry, bucket) do
+      {:ok, pid} -> callback.(pid)
+      :error -> {:error, :not_found}
+    end
   end
 end
