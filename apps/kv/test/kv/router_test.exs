788254599c60fda894974984ef7e53359299d39a
Exadra37 <exadra37@gmail.com>
2019-05-10 18:33:41 +0100

    Implementing Distributed Tasks and Configuration > Routing layer: https://elixir-lang.org/getting-started/mix-otp/distributed-tasks-and-configuration.html#routing-layer
    Signed-off-by: Exadra37 <exadra37@gmail.com>

---
+++ apps/kv/test/kv/router_test.exs
@@ -0,0 +1,13 @@
+defmodule KV.RouterTest do
+  use ExUnit.Case, async: true
+
+  test "route requests across nodes" do
+    assert KV.Router.route("hello", Kernel, :node, []) == :foo@kv
+    assert KV.Router.route("world", Kernel, :node, []) == :bar@kv
+  end
+
+  test "raises on unknown entries" do
+    assert_raise RuntimeError, ~r/could not find entry/, fn ->
+      KV.Router.route(<<0>>, Kernel, :node, []) end
+  end
+end
